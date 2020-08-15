ffebd9ec2e08f221a210b482e5e789942e553e85
fhmanwar <fhmianwar@gmail.com>
2020-08-15 14:55:53 +0700

    first Supplier

---
+++ CRUDWPF/Context/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using CRUDWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDWPF.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("CRUDWPF") { }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Item> Items { get; set; }
+    }
+}
